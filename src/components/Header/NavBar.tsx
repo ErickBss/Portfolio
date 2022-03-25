ce94ec179cade66674f842aa944ebcf389d6492a
ErickBss <erickbasso22@gmail.com>
2022-03-25 15:14:54 -0300

    drawer_button

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -1,11 +1,18 @@
-import { Flex, Link } from '@chakra-ui/react'
+import { Flex, Link, useBreakpointValue } from '@chakra-ui/react'
 
 export function NavBar() {
+  const isWideVersion = useBreakpointValue({
+    base: true,
+    md: false,
+  })
+
   return (
     <Flex
+      flexDirection={{ base: 'column', md: 'row' }}
       justify="space-between"
+      gap={{ base: '2rem', md: 0 }}
       fontWeight={600}
-      w="28rem"
+      w={{ base: 'auto', md: '28rem' }}
       fontSize="1.125rem"
     >
       <Link color="purple.400">Home</Link>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,68 @@
-import { Flex, Text } from '@chakra-ui/react'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Icon,
+  IconButton,
+  Text,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react'
 import { NavBar } from './NavBar'
 
+import { RiMenuLine } from 'react-icons/ri'
+
 export function Header() {
+  const isDrawerNavBar = useBreakpointValue({
+    base: true,
+    md: false,
+  })
+
+  const { isOpen, onClose, onOpen } = useDisclosure()
+
+  if (isDrawerNavBar) {
+    return (
+      <Flex
+        margin="0 auto"
+        justify="space-between"
+        alignItems="center"
+        h="5rem"
+        w={{ base: '30rem', md: '54rem', lg: '64rem', xl: '70rem' }}
+      >
+        <Text fontWeight={600} color="purple.400" fontSize="1.75rem">
+          ErickBss
+        </Text>
+
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="2rem"
+          variant="unstyled"
+          onClick={onOpen}
+          mt={2}
+          mr={2}
+        />
+
+        <Drawer isOpen={isOpen} placement="right" onClose={() => onClose()}>
+          <DrawerOverlay>
+            <DrawerContent bg="gray.900" p="1rem">
+              <DrawerCloseButton fontSize="1.25rem" mt="1.5rem" />
+
+              <DrawerHeader fontSize="1.5rem">Navigation</DrawerHeader>
+
+              <DrawerBody>
+                <NavBar />
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
+      </Flex>
+    )
+  }
   return (
     <Flex
       margin="0 auto"
@@ -13,6 +74,7 @@ export function Header() {
       <Text fontWeight={600} color="purple.400" fontSize="1.75rem">
         ErickBss
       </Text>
+
       <NavBar />
     </Flex>
   )
